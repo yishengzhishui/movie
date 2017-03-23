0b27337531d4a895733670e22a8a8a2764734fdf
wangxing <18912540921@163.com>
2017-03-23 15:24:05 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
     resources :reviews
 
   end
+
+  namespace :account do
+    resources :groups
+    resources :reviews
+  end
+
+
+
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
