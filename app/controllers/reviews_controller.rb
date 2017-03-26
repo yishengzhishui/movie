50145b4ea886de2d6deca453a999da7783720861
wangxing <18912540921@163.com>
2017-03-26 10:52:51 +0800

    update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new,:create]
+  before_action :authenticate_user!, :only => [:new,:create,:edit,:update,:destroy]
   def new
     @group =Group.find(params[:group_id])
     @review =Review.new
