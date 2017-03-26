572b867932e718d7e8e6f734a4ef35012aa040b6
wangxing <18912540921@163.com>
2017-03-26 15:12:51 +0800

    add My favourttws page

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,6 +2,6 @@ class Account::GroupsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @groups = current_user.participated_groups
+    @groups = current_user.participated_groups.paginate(:page => params(:page), per_page =>5 )
   end
 end
--- app/controllers/account/reviews_controller.rb
+++ app/controllers/account/reviews_controller.rb
@@ -1,6 +1,6 @@
 class Account::ReviewsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @reviews = current_user.reviews
+    @reviews = current_user.reviews.paginate(:page => params[:page], :per_page =>5)
   end
 end
