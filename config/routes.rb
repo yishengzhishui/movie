78f21af3fcae2b3f157bbe6f74d17a8833953ba0
wangxing <18912540921@163.com>
2017-03-28 13:35:29 +0800

    welcome#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+
+
   devise_for :users
   resources :groups do
     member do
@@ -14,8 +17,9 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resources :welcome do
+  end
 
-
-  root 'groups#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
